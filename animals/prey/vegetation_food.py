ae8341cbb0ecc472c9b5ee4f96245c19c084511d
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-04-23 14:49:25 -0500

    closed #14

--- animals/prey/vegetation_food.py
+++ animals/prey/vegetation_food.py
@@ -1,2 +1,8 @@
-class VegetationFood():
-    pass
+class VegitationFood():
+    def __init__(self):
+        self.prey = {"Akia Kuahiwi", "Okolemakili", "Akia", "Ilima"}
+
+    # Ilima is a shrub
+    # Akia Kuahiwi's leaves
+    # okolemakili's Seeds
+    # Akia's Fruit
---
+++ test_animals.py
@@ -0,0 +1,15 @@
+import unittest
+import arboretum
+import sys
+
+
+class TestAnimals(unittest.TestCase):
+
+    def test_river_dolphin(self):
+        
+
+
+if __name__ == '__main__':
+    sys.exit(unittest.main(sys.argv))
+
+    
