21503b8d1842ea2b1b0540f38fb5cd1c419dab56
Andrew Green <green.andrew.b@gmail.com>
2020-04-24 13:58:50 -0500

    save to review different branch

--- actions/report.py
+++ actions/report.py
@@ -1,6 +1,26 @@
 # Andrew
-def build_facility_report(arboretum):
-    for river in arboretum.rivers:
-        print(f'River [{river.id}]')
+def build_facility_report(arboretum, menu):
+    for coastline in arboretum.coastlines:
+        print(f'\nCoastline [{str(coastline.id)[:8]}]')
 
-    input("\n\nPress any key to continue...")
+    for forest in arboretum.forests:
+        print(f'\nForest [{str(forest.id)[:8]}]')
+
+    for grassland in arboretum.grasslands:
+        print(f'\nGrassland [{str(grassland.id)[:8]}]')
+
+    for mountain in arboretum.mountains:
+        print(f'\nMountain [{str(mountain.id)[:8]}]')
+
+    for river in arboretum.rivers:
+        print(f'\nRiver [{str(river.id)[:8]}]')
+        
+    for swamp in arboretum.swamps:
+        print(f'\nSwamp [{str(swamp.id)[:8]}]')
+
+    # input("\n\nPress any key to continue...")
+
+    choice = input("\nPress Enter to go back to main menu")
+
+    if choice == "":
+        menu()
--- index.py
+++ index.py
@@ -8,12 +8,16 @@ keahua = Arboretum("Keahua Arboretum", "123 Paukauila Lane")
 
 def build_menu():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("1. Annex Habitat")
-    print("2. Release Animal into Habitat")
+    print(" +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+")
+    print(" |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |")
+    print(" +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+")
+    print("\n1. Annex Biome")
+    print("2. Release New Animal")
     print("3. Feed Animal")
-    print("4. Add Plant to Habitat")
-    print("5. Display Facility Report")
+    print("4. Cultivate New Plant")
+    print("5. Show Arboretum Report")
     print("6. Exit")
+    print("\nChoose a KILLER option.")
 
 
 def main_menu():
@@ -39,7 +43,7 @@ def main_menu():
     # cultivate
 
     if choice == "5":
-        build_facility_report(keahua)
+        build_facility_report(keahua, main_menu)
         pass
 
     # what if == 6 ? ... close the program
