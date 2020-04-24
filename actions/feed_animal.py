839efdd075d59f6bf60f18e15422bde781da9e86
Dustin Murdock <dmurdock2@gmail.com>
2020-04-24 09:36:15 -0500

    started build of feeding menu

--- actions/feed_animal.py
+++ actions/feed_animal.py
@@ -1,1 +1,11 @@
 # Dustin
+import os
+import animals
+
+
+
+def feeding_menu(all_animals):
+    os.sys('cls' if os.name == 'nt' else 'clear')
+    
+    for index, animal in enumerate(all_animals):
+        
